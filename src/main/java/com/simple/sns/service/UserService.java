a53a6dfcbb62718a1e969494bd42a587a17b887c
jongha <bgh9197@naver.com>
2023-02-17 00:23:25 +0900

    feat : encode password and auth config

--- src/main/java/com/simple/sns/service/UserService.java
+++ src/main/java/com/simple/sns/service/UserService.java
@@ -1,5 +1,6 @@
 package com.simple.sns.service;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.simple.sns.exception.ErrorCode;
@@ -15,6 +16,7 @@ import lombok.RequiredArgsConstructor;
 public class UserService {
 
 	private final UserEntityRepository userEntityRepository;
+	private final BCryptPasswordEncoder encoder;
 
 	public User join(String userName, String password) {
 		// 회원가입하려는 userName으로 회원가입된 user가 있는지
@@ -23,7 +25,7 @@ public class UserService {
 		});
 
 		// 회원가입 진행 = user를 등록
-		UserEntity userEntity = userEntityRepository.save(UserEntity.of(userName, password));
+		UserEntity userEntity = userEntityRepository.save(UserEntity.of(userName, encoder.encode(password)));
 
 		return User.fromEntity(userEntity);
 	}
